ea13e2130f61644f9115d3a910aed3478e63a8e1
Ondřej Fíla <114986357+ondrejfila6969@users.noreply.github.com>
2025-04-27 17:21:30 +0200

    AbortError fixed, hotovo <3<3 <3

--- res/js/gameSettings/audio/audio.js
+++ res/js/gameSettings/audio/audio.js
@@ -8,9 +8,13 @@ const musicArray = [
 ];
 const audio = new Audio();
 const playMusic = async () => {
+    audio.pause();
     audio.src = musicArray[pacman.currentLevel - 1];
     audio.loop = true;
     audio.volume = 0.5;
+    await new Promise((resolve) => {
+        audio.addEventListener('loadeddata', resolve, { once: true });
+    });
     audio.play();
 };
 export { musicArray, audio, playMusic };
--- res/ts/gameSettings/audio/audio.ts
+++ res/ts/gameSettings/audio/audio.ts
@@ -10,9 +10,15 @@ const musicArray = [
 const audio = new Audio();
 
 const playMusic = async () => {
+  audio.pause();
   audio.src = musicArray[pacman.currentLevel - 1];
   audio.loop = true;
   audio.volume = 0.5;
+  
+  await new Promise((resolve) => {
+    audio.addEventListener('loadeddata', resolve, { once: true });
+  });
+
   audio.play();
 };
 
